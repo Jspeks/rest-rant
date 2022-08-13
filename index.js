6c4eebaf9a3337a6e481526ea750bb0535b657f2
unknown <jessespeks@hotmail.com>
2022-08-13 08:56:22 -0400

    Adding home page view

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// entered this after creating views folder
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
+// app.get('/', (req, res) => {
+//     res.send('Hello world!')
+// })
+// above was to display hello world upon start, below is to run a home page
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+  res.render('home')
 })
+
 
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
