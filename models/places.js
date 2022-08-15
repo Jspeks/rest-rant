37cb846bfc576eed981cc246a9da942311d1c216
unknown <jessespeks@hotmail.com>
2022-08-15 19:36:53 -0400

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+        name: 'Mitsuwa',
+        city: 'Edgewater',
+        state: 'NJ',
+        cuisines: 'Japanese',
+        pic: '/images/omuRice.jpg'
+        }, {
+        name: 'Jimmy Johns',
+        city: 'Herriman',
+        state: 'UT',
+        cuisines: 'Sandwich, Bakery',
+        pic: '/images/jimmyJohns.jpg'
+        }]
